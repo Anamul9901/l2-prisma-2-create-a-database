9a3d3b81507eb9356024835f243a69cb64d89e56
Anamul Haque <anamulhaque9901@gmail.com>
2024-10-30 13:50:39 +0600

    advance filtering

---
+++ src/filtering.ts
@@ -0,0 +1,41 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const filtering = async () => {
+
+    const userNameArray = ["user1", "user2", "user3"];
+
+    // aikhane 'userNameArray' er je je data name er sahte milbe se user gulu fetch korbe.
+    // const userNamesByArray = await prisma.user.findMany({
+    //     where: {
+    //         username: {
+    //             in: userNameArray
+    //         }
+    //     }
+    // })
+    // console.log(userNamesByArray);
+
+
+    // find all relation data.
+    const inDepthData = await prisma.user.findUnique({
+        where: {
+            id: 3
+        },
+        include: {
+            post: {
+                include: {
+                    postCategory: {
+                        include: {
+                            category: true
+                        }
+                    }
+                }
+            }
+        }
+    })
+    // console.log(inDepthData);
+    console.dir(inDepthData, {depth: Infinity})
+}
+
+filtering();
